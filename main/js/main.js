e8bbdf7ea0778e954a5ff9c7a558af9201b61599
SIK <daesik99@naver.com>
2024-10-22 14:59:14 +0900

    modifying

--- main/js/main.js
+++ main/js/main.js
@@ -1,19 +1,28 @@
 $(()=>{
-    /* gsap effect */
+    /* QA 배경이미지 애니메이션 */
     gsap.registerPlugin(ScrollTrigger);
-
     gsap.fromTo('.ob-box.left', {
         x: '-57%',
     },{
         x: '0%',
         duration: 2,
-        ease: "bounce.out"
+        ease: "bounce.out",
     });
     gsap.fromTo('.ob-box.right', {
         x: '30%',
     },{
         x: '0%',
         duration: 2,
-        ease: "bounce.out"
+        ease: "bounce.out",
+    });
+
+
+    /* QA 콘텐츠 애니메이션 */
+    gsap.fromTo('section.qa-content-wrap .container', {
+        opacity: 0,
+    },{
+        opacity: 1,
+        duration: .6,
+        delay: 1,
     });
 });
