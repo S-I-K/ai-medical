394d01ca94fa632b5d1cad0b84d8f87751e271b2
SIK <daesik99@naver.com>
2024-10-28 17:36:26 +0900

    modifying

--- talk-03/js/talk-03.js
+++ talk-03/js/talk-03.js
@@ -2,27 +2,27 @@ $(()=>{
     /* QA 배경이미지 인트로 애니메이션 */
     gsap.registerPlugin(ScrollTrigger);
     gsap.fromTo('.ob-box.left', {
-        x: '-57%',
-    },{
         x: '0%',
+    },{
+        x: '-66%',
         duration: 2,
         ease: "bounce.out",
     });
     gsap.fromTo('.ob-box.right', {
-        x: '30%',
-    },{
         x: '0%',
+    },{
+        x: '44%',
         duration: 2,
         ease: "bounce.out",
     });
 
 
     /* QA 콘텐츠 인트로 애니메이션 */
-    gsap.fromTo('section.qa-content-wrap .container', {
+    gsap.fromTo('section.talk-03-content-wrap .container', {
         opacity: 0,
     },{
         opacity: 1,
         duration: .6,
-        delay: 1,
+        delay: 1.4,
     });
 });
