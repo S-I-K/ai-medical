ed82d3afec4b8612eb07c3eb15519b5db9d9a6c4
SIK <daesik99@naver.com>
2024-11-08 16:49:18 +0900

    add: tutorial javascript write

--- common/js/common.js
+++ common/js/common.js
@@ -119,21 +119,27 @@ $(()=>{
 
 
     /* ============================
-        GUIDE.JS
+        LEARNING.JS
     ============================ */
+
+    /* 통역 학습 페이지 가이드 검정 스크린 페이드 */
     gsap.fromTo('.guide-screen-box', {
         opacity: 0,
     },{
         opacity: 1,
         duration: .6,
         delay: 2,
+        onComplete: () => {
+            $('.message-content-wrap.trans').addClass("guide");
+            $('.message-content-wrap.trans .button-box.sst').addClass("focus");
+        }
     });
 
-    setTimeout(function() {
+    /* setTimeout(function() {
         $('.guide-screen-box').css({
             'opacity': '0',
         })
-    }, 4000);
+    }, 4000); */
 
 
 
